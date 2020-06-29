5c9b458f852f80e8a1ee4b4c45dda131a29b0006
William Han Findiesen <william.findiesen@gmail.com>
2020-06-29 07:00:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+end
+
+fido = Dog.new
+snoopy = Dog.new
+snoopy
+
+# snoopy and fido are two different variables pointing at separate instances of the Dog class.
+
+lassie = Dog.new
+lassie
